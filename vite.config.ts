0bc7fb25ef6b9ce6de4dcee0525e9693288888dc
Ferran <ferranfusalba@gmail.com>
2023-04-10 18:40:16 +0200

    Add import path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,7 @@ export default defineConfig({
     react(),
     VitePWA({ registerType: 'autoUpdate' })
   ],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 })
